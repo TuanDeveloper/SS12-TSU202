dff314f1313cc848f10623ed9b1b72e8887df08d
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-03-16 19:31:15 +0700

    var

---
+++ script.js
@@ -0,0 +1,23 @@
+// var age = 30;
+// Khai bao lai
+// var age = "Tôi 40 tuổi";
+// Gán lại giá trị
+// age = "Tôi hiện tại đang 50 tuổi";
+// console.log(age);
+// Phạm vi - Scope
+
+// Block scope
+
+{
+    const age = 10;
+    
+}
+console.log(age);
+
+// Function scope
+// function check() {
+//     var age = 10;
+// }
+
+// console.log(age);
+
